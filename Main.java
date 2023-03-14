62694e8fd325ebd3956d2095c3de7e86abda7379
loris <loris_labarre@outlook.fr>
2023-03-14 11:51:52 +0100

    test

--- Main.java
+++ Main.java
@@ -2,16 +2,23 @@ public class Main {
     /***
      * Taille des lignes du plateau
      * */
-    final static int ROW_SIZE=7;
+    final static int ROW_SIZE=7; // colonnes
     /**
      * Taille des colonnes du plateau
      */
-    final static int COL_SIZE=6;
+    final static int COL_SIZE=6; // ligne
     /**
      * Plateau de jeu
      */
     static char[][] board=new char[COL_SIZE][ROW_SIZE];
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+
+    public static placementJeton(int positionJeu){
+        for(i=0; i<ROW_SIZE; i++){
+            System.out.println(i);
+        }
     }
 }
