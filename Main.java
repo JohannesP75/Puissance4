6392693b15d5e20ef66997f765fd1db05c328219
JohannesP75 <jpoualeu@yahoo.fr>
2023-03-14 10:06:09 +0100

    Création fichier et module Main

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    /***
+     * Taille des lignes du plateau
+     * */
+    final static int ROW_SIZE=7;
+    /**
+     * Taille des colonnes du plateau
+     */
+    final static int COL_SIZE=6;
+    /**
+     * Plateau de jeu
+     */
+    static char[][] board=new char[COL_SIZE][ROW_SIZE];
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
